2b517d61e369aeec653a111968ccc8dd1dd8bb66
lukaskrnl <lukas.ds@kupfernagel.com>
2025-02-28 21:47:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,29 +6,35 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// GeoJSON-Datei laden und Polygone anzeigen
+// Funktion zur Farbgebung je nach Baujahr
+function getColor(baujahr) {
+    return baujahr < 1800 ? '#8B0000' :  // Dunkelrot für Gebäude vor 1800
+           baujahr < 1900 ? '#FF4500' :  // Orange für 1800–1899
+           '#32CD32';                    // Grün für 1900 und neuer
+}
+
+// GeoJSON mit Multipolygonen laden
 fetch('data/gebaeude.geojson')
     .then(response => response.json())
     .then(data => {
         L.geoJSON(data, {
             style: function (feature) {
                 return {
-                    color: 'blue',
-                    fillColor: 'lightblue',
-                    fillOpacity: 0.5
+                    color: 'black',
+                    fillColor: getColor(feature.properties.baujahr),
+                    fillOpacity: 0.6,
+                    weight: 1
                 };
             },
             onEachFeature: function (feature, layer) {
                 var props = feature.properties;
                 var popupContent = `
-                    <h3>${props.name}</h3>
+                    <h3>Adresse: ${props.adresse}</h3>
                     <p><strong>Baujahr:</strong> ${props.baujahr}</p>
-                    <p><strong>Bewohner:</strong> ${props.bewohner.join(', ')}</p>
-                    <p><strong>Geschäfte:</strong> ${props.geschaefte.join(', ')}</p>
-                    <p>${props.historie}</p>
                 `;
                 layer.bindPopup(popupContent);
             }
         }).addTo(map);
     })
     .catch(error => console.error('Fehler beim Laden der GeoJSON-Daten:', error));
+
